87636d2a6f597a5b89c2f64f10442ce12c977341
JurekZela <122160360+JurekZela@users.noreply.github.com>
2023-11-11 14:26:08 +0100

    add styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,37 @@
-import { Basic } from './Phonebook/Phonebook';
+import { Formik } from 'formik';
+import { nanoid } from 'nanoid'
+import { Button,  StyledForm, StyledField, StyledLabel } from './Phonebook/Phonebook-styled';
+
 
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      <Basic />
-    </div>
-  );
-};
+      <Formik
+        initialValues={{
+          name: '',
+          number: '',
+          contacts: [],
+        }}
+        onSubmit={async (values, actions) => {
+          await new Promise((r) => setTimeout(r, 500));
+          alert(JSON.stringify(values, null, 2));
+          values.id = nanoid();
+          actions.resetForm();
+          this.contacts.push(...values)
+        }}
+      >
+        <StyledForm>
+          <StyledLabel>
+            Name
+            <StyledField  name="name" placeholder="Phil Collins" required  />
+            </StyledLabel>
+  
+          <StyledLabel>
+            Number
+            <StyledField type="number"  name="number" placeholder="+48-000-000-000" required  />
+            </StyledLabel>       
+
+          <Button type="submit">Add contact</Button>
+        </StyledForm>    
+      </Formik>
+  )
+      };
--- src/components/Phonebook/Phonebook-styled.js
+++ src/components/Phonebook/Phonebook-styled.js
@@ -1,9 +1,29 @@
 import { styled } from 'styled-components';
+import { Form, Field } from 'formik';
 
 export const Button = styled.button`
 padding: 5px;
+border: 1px solid black;
 &:hover {
     background-color: blue;
     color: black;
 }
+`;
+
+export const StyledForm = styled(Form)`
+display: flex;
+flex-direction: column;
+gap: 8px;
+max-width: 320px;
+`;
+
+export const StyledField = styled(Field)`
+padding: 4px;
+font-size: inherit;
+`;
+
+export const StyledLabel = styled.label`
+display: flex;
+flex-direction: column;
+gap: 4px;
 `;
